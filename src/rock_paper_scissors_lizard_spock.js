f88159a914f56ea72c98a8e2e8933efa91eec097
Ricard_MBP16 <rikitzzz@gmail.com>
2020-12-09 21:30:37 +0100

    fix: improved on two problems
    Improved the solution for Rock, Paper, Scissors, Lizard, Spock and for
    the search race.

--- src/rock_paper_scissors_lizard_spock.js
+++ src/rock_paper_scissors_lizard_spock.js
@@ -3,46 +3,163 @@
  * the standard input according to the problem statement.
  **/
 
-//match outcomes
-const TIE = 0;
-const WIN = 1;
-const LOSE = -1;
+//match outcomes (enum like object)
+const MatchResult = {
+  WIN: 1,
+  TIE: 0,
+  LOSE: -1
+};
 
+/**
+ * A contest represent an entire competition of Rock, Paper, Scissors, Lizard,
+ * Spock.
+ *
+ * It requires a set of players, a set bigger than 1. All players must be
+ * instances of the Player class.
+ *
+ * The contest ends when there is only one player left.
+ */
+class Contest {
+  constructor(players) {
+    if (
+      !Array.isArray(players) ||
+      players.length <= 1 ||
+      players.some((p) => p.constructor !== Player)
+    ) {
+      throw new Error('Invalid players. Must be an array with at least 2 elements!');
+    }
+
+    //Keep the original array of players
+    this.players = players.slice();
+    //Make a copy that will mutate to know who is still kicking ass
+    this.contestants = this.players.slice();
+  }
+
+  get isFinished() {
+    return this.contestants.length === 1;
+  }
+
+  get winner() {
+    while (!this.isFinished) {
+      this.playRound();
+    }
+
+    return this.contestants[0];
+  }
+
+  playRound() {
+    const winners = [];
+
+    for (let index = 0; index < this.contestants.length; index += 2) {
+      const a = this.contestants[index];
+      const b = this.contestants[index + 1];
+
+      winners.push(new Match(a, b).winner);
+    }
+
+    this.contestants = winners;
+  }
+}
+
+/**
+ * A match needs two players to be played. Each player will hold a reference
+ * to this match.
+ *
+ * The match will be played when '.play()' is called, or when the winner or the
+ * loser are accessed.
+ *
+ * There are no ties in a match. There are only ties in fights between
+ * characters, which are then resolved by the players.
+ */
+class Match {
+  constructor(a, b) {
+    if (a.constructor !== Player || b.constructor !== Player) {
+      throw new TypeError('Instances of the Player class must be provided!');
+    }
+
+    this.a = a;
+    this.b = b;
+    this._winner = null;
+    this._loser = null;
+
+    this.a.matches.push(this);
+    this.b.matches.push(this);
+  }
+
+  get winner() {
+    if (this._winner === null) {
+      this.play();
+    }
+    return this._winner;
+  }
+
+  get loser() {
+    if (this._loser === null) {
+      this.play();
+    }
+
+    return this._loser;
+  }
+
+  play() {
+    if (this._winner !== null && this._loser !== null) {
+      return;
+    }
+
+    const result = this.a.fight(this.b);
+
+    if (result === MatchResult.WIN) {
+      [this._winner, this._loser] = [this.a, this.b];
+    } else {
+      [this._winner, this._loser] = [this.b, this.a];
+    }
+  }
+}
+
+/**
+ * A Player is a participant of a Rock, Paper, Scissors, Lizard, Spock contest.
+ * It has a number and a Character.
+ *
+ * It can fight other players. The serialization algorithm prints the id of this
+ * player in one line, the id's of the players that lost against him in a
+ * different one.
+ */
 class Player {
   constructor(number, character) {
     this.number = number;
     this.character = character;
-    this.wins = [];
+    this.matches = [];
   }
 
   fight(player) {
+    if (player.constructor !== Player) {
+      throw new TypeError('Instances of Player can only fight other instances of Player!');
+    }
     //make characters fight
     const result = this.character.fight(player.character);
 
-    //process character fight result
-    if (result === WIN) {
-      this.wins.push(player.number);
-      return WIN;
-    } else if (result === LOSE) {
-      player.wins.push(this.number);
-      return LOSE;
+    //if TIE, resolve with player number
+    if (result === MatchResult.TIE) {
+      return this.number < player.number ? MatchResult.WIN : MatchResult.LOSE;
     }
 
-    //resolve TIE by checking numbers
-    if (this.number < player.number) {
-      this.wins.push(player.number);
-      return WIN;
-    }
-
-    player.wins.push(this.number);
-    return LOSE;
+    //otherwise just return the result of the fight
+    return result;
   }
 
   toString() {
-    return `${this.number}\n${this.wins.join(' ')}`;
+    return `${this.number}\n${this.matches.map((m) => m.loser.number).join(' ')}`;
   }
 }
 
+/** The Character class represents one of the characters of a Rock, Paper,
+ * Scissors, Lizard, Spock contest.
+ *
+ * It can fight other characters. The fight can tie.
+ *
+ * Get instances of this class through the CharacterFactory class.
+ *
+ */
 class Character {
   constructor(id, name, winsOver) {
     this.id = id;
@@ -52,21 +169,28 @@ class Character {
   }
 
   fight(character) {
+    if (character.constructor !== Character) {
+      throw new TypeError('Instances of Character can only fight other instances of Character!');
+    }
+
     if (character.id === this.id) {
-      return TIE; // ties
+      return MatchResult.TIE;
     }
 
     if (this.winsOver.includes(character.id)) {
-      return WIN; // rock wins
+      return MatchResult.WIN;
     }
-    return LOSE; // rock loses
+    return MatchResult.LOSE;
   }
 }
 
+/**
+ * Returns concrete instances of Characters from the identifier of the Character.
+ */
 class CharacterFactory {
   constructor() {}
 
-  static get(characterId) {
+  static make(id) {
     //characters
     const ROCK = 'R';
     const PAPER = 'P';
@@ -74,7 +198,7 @@ class CharacterFactory {
     const LIZARD = 'L';
     const SPOCK = 'S';
 
-    switch (characterId) {
+    switch (id) {
       case ROCK:
         return new Character(ROCK, 'Rock', [LIZARD, SCISSORS]);
       case PAPER:
@@ -86,7 +210,7 @@ class CharacterFactory {
       case SPOCK:
         return new Character(SPOCK, 'Lizard', [SCISSORS, ROCK]);
       default:
-        throw new Error(`Unknown characterId: ${characterId}`);
+        throw new Error(`Unknown characterId: ${id}`);
     }
   }
 }
@@ -96,27 +220,10 @@ const players = [];
 for (let i = 0; i < N; i++) {
   var inputs = readline().split(' ');
   const numPlayer = parseInt(inputs[0]);
-  const character = CharacterFactory.get(inputs[1]);
+  const character = CharacterFactory.make(inputs[1]);
   players.push(new Player(numPlayer, character));
 }
 
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-function whoIsTheWinner(players) {
-  const contestants = players.slice();
-  for (let index = 0; contestants.length !== 1; index = (index + 1) % contestants.length) {
-    const a = contestants[index];
-    const b = contestants[index + 1];
+const contest = new Contest(players);
 
-    const matchResult = a.fight(b);
-    if (matchResult === WIN) {
-      contestants.splice(index + 1, 1);
-    } else {
-      contestants.splice(index, 1);
-    }
-  }
-
-  return contestants[0];
-}
-
-console.log(whoIsTheWinner(players).toString());
+console.log(contest.winner.toString());
--- src/search_race.js
+++ src/search_race.js
@@ -37,60 +37,47 @@ while (isInGame()) {
   // To debug: console.error('Debug messages...');
   const angleToTarget = getAngle(car, target);
   const distance = dist(car, target);
+  const thrustAndModulation = calculateThrust(angle, angleToTarget, distance);
   console.error({
     angle,
     angleToTarget,
     angleDiff: Math.abs(angle - angleToTarget),
     distance,
-    checkPointsRatio: distance / 600
+    checkPointsRatio: distance / 600,
+    ...thrustAndModulation
   });
 
-  const thrust = calculateThrust(angle, angleToTarget, distance);
-  console.log(`${target.x} ${target.y} ${thrust} ${thrust}`); // X Y THRUST MESSAGE
+  console.log(
+    `${target.x} ${target.y} ${thrustAndModulation.thrust} ${thrustAndModulation.thrust}`
+  ); // X Y THRUST MESSAGE
 }
 
 function calculateThrust(carAngle, angleToTarget, distance) {
-  const maxThrust = 180;
+  const maxThrust = 195;
   const angleDiff = Math.abs(carAngle - angleToTarget);
   const checkpointWidth = 600;
   let thrust = maxThrust;
 
-  const far = 8 * checkpointWidth;
-  const close = 6 * checkpointWidth;
+  const slow = 60;
 
-  const slow = 45;
+  const angleModulation = Math.pow(1 - angleDiff / 360, 2);
 
-  //if very far
-  if (distance >= far) {
-    //increase speed as the car aligns with the target
-    const modulation = 1 - angleDiff / 360;
-    console.error('far', { modulation });
-    thrust = maxThrust * modulation;
-  }
-
-  //if medium distance
-  if (distance < far && distance > close) {
-    //reduce speed the closer to the target, but increase it the more aligned
-    const modulation = (distance / (2 * far)) * Math.pow(1 - angleDiff / 360, 2);
-    console.error('medium', { modulation });
-    thrust = maxThrust * modulation;
-  }
-
-  //if close
-  if (distance < close) {
-    //reduce speed to closer you get to the target but increase the more aligned
-    const modulation = (distance / (2 * far)) * Math.pow(1 - angleDiff / 360, 2);
-    console.error('close', { modulation });
-    thrust = maxThrust * modulation;
-  }
+  const distanceModulation = Math.pow(distance / (10 * checkpointWidth), 2);
+  const cappedDistanceModulation = Math.min(1, Math.max(0, distanceModulation));
+  //reduce speed to closer you get to the target but increase the more aligned
+  const modulation = cappedDistanceModulation * angleModulation;
+  thrust = maxThrust * modulation;
 
   //if speed is lower than 45, adjust according to angle
   if (thrust < slow) {
-    console.error('too slow', { thrust });
     //unless the car is pretty much aligned with the goal
     thrust = slow;
   }
-  return thrust.toFixed(0);
+
+  return {
+    thrust: thrust.toFixed(0),
+    modulation
+  };
 }
 
 function dist(a, b) {
@@ -109,7 +96,7 @@ function getAngle(car, target) {
       return 180 + rawAngle; //3rd quadrant
     }
   } else {
-    if (car.y > target.y) {
+    if (car.y >= target.y) {
       return rawAngle; //1st quadrant
     } else {
       return 270 + rawAngle; //4th quadrant
